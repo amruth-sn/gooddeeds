87c925f5e2bcb2327c2ed2e09b0411dd45cbd4bf
Andrew Sun <sun.andr@northeastern.edu>
2024-11-02 15:12:43 -0400

    as

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,7 @@
-from flask import Flask
 import os
-import pyodbc
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://mubinmodi%40gooddeed6969:Cjwl@134@gooddeed6969.database.windows.net:1433/GoodDeed?"
-        "driver=ODBC+Driver+17+for+SQL+Server;"
-        "Encrypt=yes;"
-        "TrustServerCertificate=no;"
-        "Connection Timeout=30;"
-        "hostNameInCertificate=*.database.windows.net;"
+        "postgresql://db_owner:tYZ6kchHL5nP@ep-quiet-firefly-a4jwnluc.us-east-1.aws.neon.tech/db?sslmode=require"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
