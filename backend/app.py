0aebea657f8831cc6f1b0e61d0a91b2ee5317d1d
Rehan Khan <khan.reh@northeastern.edu>
2024-11-03 07:46:28 -0500

    updated the render url

--- backend/app.py
+++ backend/app.py
@@ -43,5 +43,5 @@ api.add_resource(ChatbotResource, '/chatbot/<int:event_id>')
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0')
+    # app.run(debug=True, port=8080)
